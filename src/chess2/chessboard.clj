9f1ff7834ac109663e8fd0620433be99aa13c507
QuietOne <tihomir.radosavljevic@gmail.com>
2015-01-02 13:15:13 +0100

    general move logic done

--- src/chess2/chessboard.clj
+++ src/chess2/chessboard.clj
@@ -52,12 +52,6 @@
     (valid-y? y)
     (valid-y? (calc-offset-y y offset))))
 
-(defn valid-movement [chessboard fromx fromy tox toy]
-  (and 
-    (valid-pos? fromx fromy)
-    (valid-pos? tox toy)
-    (occupied? chessboard tox toy)))
-
 (defn get-piece [chessboard x y]
   (if (and (valid-pos? x y) (occupied? chessboard x y))
       (get (get chessboard x) y)))
@@ -82,3 +76,36 @@
   ([chessboard x y] (if (and (valid-pos? x y) (occupied? chessboard x y))
                       (piece-name (get-piece chessboard x y))
                       :empty)))
+
+(defn set-piece 
+ ([chessboard x y the-string] (if (valid-pos? x y)
+                                (assoc chessboard x (assoc (get chessboard x) y the-string))))
+  
+ ([chessboard x y name color] (cond
+                                (and (= color :white) (= name :pawn)) (set-piece chessboard x y "wp")
+                                (and (= color :white) (= name :rook)) (set-piece chessboard x y "wr")
+                                (and (= color :white) (= name :knight)) (set-piece chessboard x y "wn")
+                                (and (= color :white) (= name :bishop)) (set-piece chessboard x y "wb")
+                                (and (= color :white) (= name :queen)) (set-piece chessboard x y "wq")
+                                (and (= color :white) (= name :king)) (set-piece chessboard x y "wk")
+                                (and (= color :black) (= name :pawn)) (set-piece chessboard x y "bp")
+                                (and (= color :black) (= name :rook)) (set-piece chessboard x y "br")
+                                (and (= color :black) (= name :knight)) (set-piece chessboard x y "bn")
+                                (and (= color :black) (= name :bishop)) (set-piece chessboard x y "bb")
+                                (and (= color :black) (= name :queen)) (set-piece chessboard x y "bq")
+                                (and (= color :black) (= name :king)) (set-piece chessboard x y "bk")
+                                :else chessboard)))
+
+(defn remove-piece [chessboard x y]
+  (set-piece chessboard x y "."))
+
+(defn valid-movement? [chessboard fromx fromy tox toy]
+  (and 
+    (valid-pos? fromx fromy)
+    (valid-pos? tox toy)))
+
+(defn move-piece [chessboard fromx fromy tox toy]
+  (if (valid-movement? chessboard fromx fromy tox toy)
+    (let [piece (get-piece chessboard fromx fromy)]
+      (set-piece (remove-piece chessboard fromx fromy) tox toy piece))
+    chessboard))
